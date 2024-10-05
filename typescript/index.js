fe5b7ac19969569aaa0ac25e620d8eec5c728f54
أمل . <kingamal001@gmail.com>
2024-10-05 17:46:26 +0100

    Add main program menu

--- typescript/index.js
+++ typescript/index.js
@@ -1,14 +1,36 @@
 // import * as readline from 'readline'
 console.clear();
+beep(440, 500);
 console.log("******************");
 console.log("Welcome to SEAP-Fi");
 console.log("******************");
 console.log("");
-console.log("Please select an option:\n \n    \n    1. Customer account and creation\n \n    2. Account opening\n \n    3. Deposit to account\n \n    4. Widthdrawal from account\n \n    5. Transfer to other accounts\n \n    6. Account closing\n \n    7. View all Transactions\n");
+console.log("Please select an option:\n \n    1. Onboard customer.\n \n    2. Open customer account.\n \n    3. Deposit to customer account.\n\n    4. Widthdraw from customer account.\n \n    5. Transfer to other accounts.\n \n    6. Close an account.\n \n    7. Exit application.\n\n    ");
 console.log("");
 console.log("option:");
 console.log("");
 console.log("");
+function beep(frequency, duration) {
+    // Create an AudioContext instance 
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    // Create an oscillator node (to generate sound) 
+    var oscillator = audioContext.createOscillator();
+    var gainNode = audioContext.createGain();
+    // Set oscillator frequency (pitch) 
+    oscillator.frequency.value = frequency;
+    // Connect the oscillator to the gain node (volume control) 
+    oscillator.connect(gainNode);
+    // Connect the gain node to the AudioContext destination (speakers) 
+    gainNode.connect(audioContext.destination);
+    // Start the oscillator 
+    oscillator.start();
+    // Stop the sound after the specified duration 
+    setTimeout(function () {
+        oscillator.stop();
+        audioContext.close();
+    }, duration);
+}
+// Usage example: Beep at 440 Hz for 500 milliseconds beep(440, 500);
 // const rl = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,24 +1,49 @@
 
 // import * as readline from 'readline'
 console.clear();
-
+// beep(440, 500);
 console.log("******************");
 console.log("Welcome to SEAP-Fi");
 console.log("******************");
 console.log("");
+
 console.log(`Please select an option:\n 
-    
-    1. Customer account and creation\n 
-    2. Account opening\n 
-    3. Deposit to account\n 
-    4. Widthdrawal from account\n 
-    5. Transfer to other accounts\n 
-    6. Account closing\n 
-    7. View all Transactions\n`);
+    1. Onboard customer.\n 
+    2. Open customer account.\n 
+    3. Deposit to customer account.\n
+    4. Widthdraw from customer account.\n 
+    5. Transfer to other accounts.\n 
+    6. Close an account.\n 
+    7. Exit application.\n
+    `);
+
 console.log("");
 console.log("option:");
 console.log("");
 console.log("");
+
+// function beep(frequency: number, duration: number): void {
+//     // Create an AudioContext instance 
+//     const audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
+//     // Create an oscillator node (to generate sound) 
+//     const oscillator = audioContext.createOscillator(); const gainNode = audioContext.createGain();
+//     // Set oscillator frequency (pitch) 
+//     oscillator.frequency.value = frequency;
+//     // Connect the oscillator to the gain node (volume control) 
+//     oscillator.connect(gainNode);
+//     // Connect the gain node to the AudioContext destination (speakers) 
+//     gainNode.connect(audioContext.destination);
+//     // Start the oscillator 
+//     oscillator.start();
+//     // Stop the sound after the specified duration 
+//     setTimeout(() => {
+//         oscillator.stop();
+//         audioContext.close();
+
+//     }, duration);
+// }
+
+// Usage example: Beep at 440 Hz for 500 milliseconds beep(440, 500);
 
 // const rl = readline.createInterface({
 //   input: process.stdin,
