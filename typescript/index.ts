89aec072f57c5e9220e5ccc78cff09005f432ebe
أمل . <kingamal001@gmail.com>
2024-10-05 17:15:26 +0100

    Add menu

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,18 +1,30 @@
+
 // import * as readline from 'readline'
 console.clear();
+
 console.log("******************");
 console.log("Welcome to SEAP-Fi");
 console.log("******************");
 console.log("");
-console.log("Please select an option:\n \n    \n    1. Customer account and creation\n \n    2. Account opening\n \n    3. Deposit to account\n \n    4. Widthdrawal from account\n \n    5. Transfer to other accounts\n \n    6. Account closing\n \n    7. View all Transactions\n");
+console.log(`Please select an option:\n 
+    
+    1. Customer account and creation\n 
+    2. Account opening\n 
+    3. Deposit to account\n 
+    4. Widthdrawal from account\n 
+    5. Transfer to other accounts\n 
+    6. Account closing\n 
+    7. View all Transactions\n`);
 console.log("");
 console.log("option:");
 console.log("");
 console.log("");
+
 // const rl = readline.createInterface({
 //   input: process.stdin,
 //   output: process.stdout
 // });
+
 // rl.question(`Please select an option\n
 //     1. Customer account and creation\n
 //     2. Account opening\n
@@ -22,6 +34,7 @@ console.log("");
 //     6. Account closing\n
 //     7. View all Transactions\n`, (answer: string) => {
 //   console.log(`${answer}!`);
+
 //   // Close the readline interface
 //   rl.close();
 // }); 
