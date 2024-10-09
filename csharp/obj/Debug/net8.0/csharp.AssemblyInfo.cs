bcb14658e6b896a13749544714675e7951b8e265
ifejika <sbmenation@gmail.com>
2024-10-09 16:23:57 +0100

    Update code

--- csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0550fe12c9e42d5e18b7299abc602da15c84b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c53912fe7df9862d434e97047bd004f7d57fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
